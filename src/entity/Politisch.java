3727b89bc566a9a4f0e00b661e1a088109998e29
abazatte <abdurrahman.azattemuer@hs-osnabrueck.de>
2022-06-17 13:54:32 +0200

    Polli

--- src/entity/Politisch.java
+++ src/entity/Politisch.java
@@ -1,4 +1,19 @@
 package entity;
 
 public class Politisch {
+    public boolean aktiv() {
+        return false;
+    }
+
+    public void parteilob() {
+    }
+
+    public void parteitadel() {
+    }
+
+    public void wirtschaftslob() {
+    }
+
+    public void erwischt() {
+    }
 }
